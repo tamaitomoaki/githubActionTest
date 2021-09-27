53216755471a322537cf42cd7f381239a83d4b96
tama <tamazusa00@gmail.com>
2021-09-27 19:12:12 +0900

    github,action,issue,test8

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -5,7 +5,8 @@ const github = require('@actions/github');
 
 const main = async () => {
   console.log('確認')
-  console.log(github.context.payload)
+  console.log(github.context.payload.issue.assignee.login)
+  console.log(github.context.payload.issue.assignees)
   console.log('確認')
 }
 
