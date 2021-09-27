34b46015975254185a5e2f3336ea25d234fd3d18
tama <tamazusa00@gmail.com>
2021-09-27 22:22:51 +0900

    github,action,issue,test29

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const main = async () => {
   console.log('確認')
-  console.log(github.context.payload.issue.assignee)
+  console.log(github.context.payload.issue)
   console.log(github.context.payload.issue.assignee.login)
   console.log(github.context.payload.issue.assignees)
   console.log('確認')
@@ -14,11 +14,10 @@ const main = async () => {
   // レビュワー取得、メンション設定
   try {
     if (github.context.payload.issue.assignees.length !== 0) {
-      const reviewer = github.context.payload.issue.assignees[0].login
-      console.log(reviewer)
-      // core.setOutput('mention', `<@${mention[reviewer]}>`);
+      const assignee = github.context.payload.issue.assignees[0].login
+      core.setOutput('mention', `<@${mention[assignee]}>`);
     } else {
-      // core.setOutput('mention', `<@U51QZQFV0>`);
+      core.setOutput('mention', `<@U51QZQFV0>`);
     }
   } catch (err) {
     console.error(err.message);
