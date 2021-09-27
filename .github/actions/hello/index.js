1c0ebddea27651dd1a28a92b80c6f00ea9ed1f9d
tama <tamazusa00@gmail.com>
2021-09-27 17:55:46 +0900

    github,action,issue,test

---
+++ .github/actions/hello/index.js
@@ -0,0 +1,12 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const path = require('path');
+const { Client } = require("@notionhq/client")
+
+const main = async () => {
+  console.log('確認')
+  console.log(github.context.payload)
+  console.log('確認')
+}
+
+main()
